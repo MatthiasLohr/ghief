2bcf79da41e02c85ad303b6485512569a9f5e1a1
Matthias Lohr <matthias@lohr.me>
2017-02-28 20:19:36 +0100

    remove stale remote branches

--- ghief/backup_git_repository.py
+++ ghief/backup_git_repository.py
@@ -8,5 +8,5 @@ def backup_git_repository(source, target):
     gitdir = tempfile.mkdtemp(suffix='.git')
     subprocess.call(['git', 'clone', '--bare', source, gitdir])
     subprocess.call(['git', 'remote', 'add', 'backup', target], cwd=gitdir)
-    subprocess.call(['git', 'push', '--all', 'backup'], cwd=gitdir)
+    subprocess.call(['git', 'push', '--all', '--prune', 'backup'], cwd=gitdir)
     shutil.rmtree(gitdir)
