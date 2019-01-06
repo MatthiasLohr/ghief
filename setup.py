41046cf0920171af671084a4979d8fdd9be22ce6
Matthias Lohr <mail@mlohr.com>
2019-01-06 14:19:13 +0100

    upgraded pyyaml concerning CVE-2017-18342

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup
 setup(
     name='ghief',
-    version='0.1.2',
+    version='0.1.3',
     author='Matthias Lohr',
     author_email='matthias@lohr.me',
     description='Git Backup Script',
     packages=['ghief'],
-    install_requires=['PyYAML'],
+    install_requires=['pyyaml>=4.2b1'],
     entry_points={
         'console_scripts': ['ghief=ghief.__main__:main']
     }
